7d085158cef577cdb018be755150b527ef4dab0c
Mosesotieno-ai <mosesotieno8363@gmail.com>
2025-06-25 01:51:18 +0300

    add my context

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginForm from "./components/auth/LoginForm";
+import RegisterForm from "./components/auth/RegisterForm";
+import PlantsPage from "./pages/PlantsPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  <Route path="/plants" element={<ProtectedRoute><PlantsPage /></ProtectedRoute>} />
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/plants" element={<PlantsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
